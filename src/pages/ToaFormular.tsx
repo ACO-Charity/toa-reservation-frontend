513455dbab330800c556ed1708d06584be162a0c
Thu Pham <ptthu@gmx.de>
2024-07-12 17:40:26 +0200

    outsourced form description

--- src/pages/ToaFormular.tsx
+++ src/pages/ToaFormular.tsx
@@ -1,18 +1,15 @@
+import FormDescription from "../components/form-description/FormDescription.tsx";
+
 function ToaFormular() {
 
 
-  return (
-    <>
-<h1>Workshop Reservierung</h1>
-<p>Erleben Sie einen spannenden Nachmittag voller kulinarischer Entdeckungen und lernen Sie die Kunst der asiatischen Küche in unseren Workshops. Wir bieten drei intensive und praxisorientierte Workshops.</p>
-<p>Zeitplan der Workshops:</p>
-
-<p>Beginn: 15:30 Uhr</p>
-<p>Dauer: Jeweils 30 Minuten pro Workshop</p>
-<p>Pause: 10 Minuten zwischen den Workshops</p>
-<p>Ende: 18:00 Uhr</p>
-    </>
-  )
+    return (
+        <>
+            <h1>Workshop Reservierung</h1>
+            <FormDescription/>
+        </>
+    )
 }
+
 
 export default ToaFormular
