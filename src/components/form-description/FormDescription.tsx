56511643fa906b3ba524fafa8201fe09e52bb75e
Thu Pham <ptthu@gmx.de>
2024-07-14 00:04:30 +0200

    Update font-sizes and line-height

--- src/components/form-description/FormDescription.tsx
+++ src/components/form-description/FormDescription.tsx
@@ -3,14 +3,16 @@ function FormDescription() {
 
     return (
         <>
-            <p>Erleben Sie einen spannenden Nachmittag voller kulinarischer Entdeckungen </p>
-            <p>und lernen Sie die Kunst der asiatischen Küche in unseren Workshops. </p>
-            <p> Wir bieten drei intensive und praxisorientierte Workshops.</p>
+            <p>Erleben Sie einen spannenden Nachmittag voller kulinarischer</p>
+            <p>Entdeckungen und lernen Sie die Kunst der asiatischen Küche </p>
+            <p>in unseren Workshops. Wir bieten drei intensive und </p>
+            <p>praxisorientierte Workshops.</p>
             <h2>Zeitplan der Workshops:</h2>
-            <p>Beginn: 15:30 Uhr</p>
-            <p>Dauer: Jeweils 30 Minuten pro Workshop</p>
-            <p>Pause: 10 Minuten zwischen den Workshops</p>
-            <p>Ende: 18:00 Uhr</p>
+            <p><strong>Datum:</strong> XX. Oktober 2024</p>
+            <p><strong>Beginn:</strong> 15:30 Uhr</p>
+            <p><strong> Dauer:</strong> Jeweils 30 Minuten pro Workshop</p>
+            <p><strong>Pause:</strong> 10 Minuten zwischen den Workshops</p>
+            <p><strong>Ende:</strong> 18:00 Uhr</p>
         </>
     )
 }
