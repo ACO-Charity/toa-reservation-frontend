12730726b352f286068ede0b4b5f182ae8b7d962
Thu Pham <ptthu@gmx.de>
2024-07-13 23:43:50 +0200

    Update font-family to Inter and adjust font colors

--- src/components/form-description/FormDescription.tsx
+++ src/components/form-description/FormDescription.tsx
@@ -3,9 +3,10 @@ function FormDescription() {
 
     return (
         <>
-            <p>Erleben Sie einen spannenden Nachmittag voller kulinarischer Entdeckungen und lernen Sie die Kunst der
-                asiatischen Küche in unseren Workshops. Wir bieten drei intensive und praxisorientierte Workshops.</p>
-            <p>Zeitplan der Workshops:</p>
+            <p>Erleben Sie einen spannenden Nachmittag voller kulinarischer Entdeckungen </p>
+            <p>und lernen Sie die Kunst der asiatischen Küche in unseren Workshops. </p>
+            <p> Wir bieten drei intensive und praxisorientierte Workshops.</p>
+            <h2>Zeitplan der Workshops:</h2>
             <p>Beginn: 15:30 Uhr</p>
             <p>Dauer: Jeweils 30 Minuten pro Workshop</p>
             <p>Pause: 10 Minuten zwischen den Workshops</p>
