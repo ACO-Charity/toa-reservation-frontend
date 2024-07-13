5f061c20616d4ab1a70148610ef764bdc77e7cc3
Pham Minh Hoang Henry <minhhoangpham@live.de>
2024-07-14 00:49:21 +0200

    feature: format description text

--- src/components/form-description/FormDescription.tsx
+++ src/components/form-description/FormDescription.tsx
@@ -1,21 +1,19 @@
+import DescriptionDetails from './description-details/DescriptionDetails.tsx';
+
 function FormDescription() {
 
-
     return (
-        <>
-            <p>Erleben Sie einen spannenden Nachmittag voller kulinarischer</p>
-            <p>Entdeckungen und lernen Sie die Kunst der asiatischen Küche </p>
-            <p>in unseren Workshops. Wir bieten drei intensive und </p>
-            <p>praxisorientierte Workshops.</p>
+        <div className="form-description">
+            <p>Erleben Sie einen spannenden Nachmittag voller kulinarischer Entdeckungen und lernen Sie die Kunst der asiatischen Küche in unseren
+                Workshops. Wir bieten drei intensive und praxisorientierte Workshops.</p>
             <h2>Zeitplan der Workshops:</h2>
-            <p><strong>Datum:</strong> XX. Oktober 2024</p>
-            <p><strong>Beginn:</strong> 15:30 Uhr</p>
-            <p><strong> Dauer:</strong> Jeweils 30 Minuten pro Workshop</p>
-            <p><strong>Pause:</strong> 10 Minuten zwischen den Workshops</p>
-            <p><strong>Ende:</strong> 18:00 Uhr</p>
-        </>
-    )
+            <DescriptionDetails category="Datum" detail="12. Oktober 2024"/>
+            <DescriptionDetails category="Beginn" detail="15:30 Uhr"/>
+            <DescriptionDetails category="Dauer" detail="Jeweils 30 Minuten pro Workshop"/>
+            <DescriptionDetails category="Pause" detail="10 Minuten zwischen den Workshops"/>
+            <DescriptionDetails category="Ende" detail="18:00 Uhr"/>
+        </div>
+    );
 }
 
-
-export default FormDescription
+export default FormDescription;
---
+++ src/components/form-description/description-details/DescriptionDetails.tsx
@@ -0,0 +1,14 @@
+import './DescriptionDetails.scss';
+
+export interface DescriptionDetailsProps {
+    category: string;
+    detail: string;
+}
+
+const DescriptionDetails = (props: DescriptionDetailsProps) => {
+    return (
+        <div className="description-details"><strong>{props.category}:</strong><span>{props.detail}</span></div>
+    );
+};
+
+export default DescriptionDetails;
